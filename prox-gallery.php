cb9c1ee090bc84721269426b7aabf95c37457d36
Marcel Santing <marcel@prox-web.nl>
2026-02-21 11:59:19 +0100

    First initial commit

---
+++ prox-gallery.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Prox Gallery
+ * Description:       Modular, service-oriented WordPress gallery plugin built with a PSR-4 architecture.
+ * Version:           0.1.0
+ * Requires at least: 6.9.1
+ * Requires PHP:      8.1
+ * Author:            Marcel Santing
+ * Author URI:        mailto:marcel@prox-web.nl
+ * License:           GPL-3.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       prox-gallery
+ *
+ * @package           ProxGallery
+ * @since             0.1.0
+ */
+
+declare(strict_types=1);
